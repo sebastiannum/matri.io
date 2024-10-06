0333d83d6bee94b158b5084809e003c17e33891e
sebastiano <32073914+sebastiannum@users.noreply.github.com>
2024-10-06 20:04:05 -0300

    Testing sitio funcional

---
+++ redirect.js
@@ -0,0 +1,15 @@
+function toSpotify() {
+    window.open("https://open.spotify.com/playlist/4l8bqsszIKA1hw3VKs5pKn?si=9qJ9WgOUTpCxUPvDBMIJxA");
+}
+
+function toForm() {
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSftsFC_EvOgJWfWoi68IyOtloHAaCfZWKDDCVrMsd5oO-MT5Q/viewform");
+}
+
+function toLocation() {
+    window.open("https://maps.app.goo.gl/XB9PcnDJt5hRqXzx7");
+}
+
+function toCalendar(){
+    window.open("https://calendar.app.google/f5qFtRn17re9Xkiq6");
+}
